8a5fe3f0ce28ae02c514c21d46bb33df31fdd0a8
mayuras7685 <mayurasodara@gmail.com>
2022-10-26 22:07:36 +0530

    charts functions

---
+++ datavis.js
@@ -0,0 +1,39 @@
+const ctx = document.getElementById("myChart").getContext("2d")
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Sun", "Mon", "Tue", "wed", "Thu", "Fri", "Sat"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [10, 5, 10, 0, 10, 5],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+})
