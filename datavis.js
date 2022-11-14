f4eaa1096a3f401521722adff4599f2f381b7290
mayuras7685 <mayurasodara@gmail.com>
2022-11-14 23:51:00 +0530

    polar chart js

--- datavis.js
+++ datavis.js
@@ -216,3 +216,41 @@ const mlChart = new Chart(ctx1, {
     },
   },
 })
+
+const DATA_COUNT = 5
+const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 }
+
+const labels = ["Red", "Orange", "Yellow", "Green", "Blue"]
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: Utils.numbers(NUMBER_CFG),
+      backgroundColor: [
+        Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+        Utils.transparentize(Utils.CHART_COLORS.orange, 0.5),
+        Utils.transparentize(Utils.CHART_COLORS.yellow, 0.5),
+        Utils.transparentize(Utils.CHART_COLORS.green, 0.5),
+        Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+      ],
+    },
+  ],
+}
+
+const config1 = {
+  type: "polarArea",
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Polar Area Chart",
+      },
+    },
+  },
+}
